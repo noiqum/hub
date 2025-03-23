b60eb1972b0d8c92b297aba45622214ddc7bd02c
noiqum <onurcoskun99@gmail.com>
2025-03-23 19:23:23 +0300

    style: color pallette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-primary-white">
+        <div><a href="">ali</a></div>
+        <div>kakaka</div>
+        <div>jajaj</div>
+        <div>ajajjaj</div>
+        <div></div>
 
 
       </main>
